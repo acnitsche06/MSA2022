d0152af2f626f4d13d17cefd1b493706c78710a3
acnitsche06 <107645388+acnitsche06@users.noreply.github.com>
2022-06-24 14:44:23 -0500

    Add files via upload

--- student_minor.py
+++ student_minor.py
@@ -1,4 +1,6 @@
 #Print the menu
+
+
 print("Select option from menu\n__________________________")
 print("1. Login")
 print("2. Create User")
@@ -11,10 +13,33 @@ while True:
         print("\nERROR: Enter a 1 or 2")
         continue
     else:
-        print("YAY")
+        print("Good job you followed directions")
         break
+
 #If user chose 1, ask for username and password; and check username and password combination in the users.txt file
-#- If not valid combination reprompt the user. If valid then move on to prompt for student data
+if user_option == "1":
+    while True:
+        user_name = input("Please enter your user name: ")
+        user_pass = input("Please enter your password: ")
+        #Open the users files
+        user_file = open("users.txt", "r")
+        user_found = False
+        for line in user_file:
+            credentials = line.split(", ")
+            #Check if username and password are valid
+            if user_name == credentials[0] and user_pass == credentials[1].rstrip():
+                #If valid, write to users.txt file and move on to prompt for student data
+                user_found = True
+                break
+
+        if user_found:
+            print(f"User {user_name} successfully logged in!\n")
+            break
+        #If not valid reprompt the user
+        else:
+            print(f"User {user_name} not found!\n")
+
+
 #If user chose 2, ask for username and password; and validate username and password length.
 #-If valid, write to users.txt file and move on
 #If not vaild reprompt user
